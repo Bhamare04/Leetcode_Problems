4e33a8c2c65707df89ddab69732caf6d6d266c3a
Kartik bhamare <148467400+Bhamare04@users.noreply.github.com>
2026-07-27 21:08:18 +0530

    Time: 3 ms (17.83%), Space: 43.1 MB (11.77%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String result = "";
+        for (int i=0;i<address.length();i++){
+            char ch = address.charAt(i);
+            if(ch == '.'){
+                result = result +"[.]";
+            }else
+            result = result + ch ;
+        }
+        return result;
+
+    }
+    
+}
